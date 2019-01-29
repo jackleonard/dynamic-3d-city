95c7713f7f6cd3f180b30c6f9a303f5da2c7b259
Jack Leonard <me@jackleonard.me>
2019-01-29 22:04:08 +0000

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/joshbader/pen/qMEbQV.
+
+ Made with mostly CSS and a bit of JS for dynamic building extrusion.
---
+++ js/cityIndex.js
@@ -0,0 +1,40 @@
+function generateBuildings() {
+  var buildings = $('.buildings');
+  
+  buildings.each(function(index) {
+    var self = $(this),
+        parentBlock = self.parent('.block'),
+        buildingsLength = (parentBlock.hasClass('double-wide')) ? 18 : 9;
+    
+    for (var i = 0; i < buildingsLength; i++) {
+      var randomDepth = Math.floor(Math.random() * 10) + 5,
+          building = $('<div class="building" data-depth="' + randomDepth + '">');
+      
+      self.append(building);
+    }
+  });
+}
+
+function extrudeBuildings() {
+  var building = $('.building');
+
+  building.each(function(index) {
+    var self = $(this),
+        depth = self.data('depth');
+
+    for (var i = 0; i < depth; i++) {
+      var extrusion = $('<span class="extrusion">');
+
+      extrusion.css({
+        'z-index': i * -1,
+        'transform': 'translate3d(0,0,' + i * 0.25 + 'vw)',
+        'z-index': i
+      });
+
+      self.append(extrusion);
+    }
+  });
+};
+
+generateBuildings();
+extrudeBuildings();
